36f2c9f4609cbc80845859ffe06ced3e8a7b4504
sonab <sonabstudios@gmail.com>
2019-11-20 11:36:40 +0800

    pdf generator

--- function/app.js
+++ function/app.js
@@ -1,25 +1,96 @@
 const fs = require('fs')
-const pdf = require('pdf-lib')
-const PDFDocument = pdf.PDFDocument
-const rgb = pdf.rgb
-const StandardFonts = pdf.StandardFonts
-const degrees = pdf.degrees
+const { pdf, PDFDocument, rgb, StandardFonts, degrees } = require('pdf-lib')
 
-let createFilename = () => {
+let createFilename = data => {
   var now = new Date()
-  var firstName = 'Firstname'
-  var lastName = 'Lastname'
-  var dateTime = `/${firstName}-${lastName}-EDSC-EMP5624-2019-10-001-E-${now.getFullYear()}-${now.getMonth() +
-    1}-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}-${now.getMilliseconds()}`
-
-  return dateTime + '.pdf'
+  return `${data.firstName}-${
+    data.lastName
+  }-EDSC-EMP5624-2019-10-001-E-${now.getFullYear()}-${now.getMonth() +
+    1}-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}-${now.getMilliseconds()}.pdf`
 }
 
-let index = async () => {
-  const uint8Array = fs.readFileSync('./ESDC-EMP5624.pdf')
+const ROW_01 = 261
+const ROW_03 = 204
+
+const COLUMN_01 = 25
+const COLUMN_02 = 190
+const COLUMN_03 = 354
+
+let writeFirstName = (page, font, firstName) => {
+  console.log('writing first name...')
+  page.drawText(firstName, {
+    x: COLUMN_01,
+    y: ROW_01,
+    size: 14
+  })
+}
+
+let writeMiddleName = (page, font, middleName) => {
+  console.log('writing middle name...')
+  page.drawText(middleName, {
+    x: COLUMN_02,
+    y: ROW_01,
+    size: 14
+  })
+}
+
+let writeLastName = (page, font, lastName) => {
+  console.log('writing last name...')
+  page.drawText(lastName, {
+    x: COLUMN_03,
+    y: ROW_01,
+    size: 14
+  })
+}
+
+let writeEmail = (page, font, email) => {
+  console.log('writing email...')
+  page.drawText(email, {
+    x: COLUMN_01,
+    y: ROW_03,
+    size: 12
+  })
+}
+
+let index = async data => {
+  // create new pdf document
+  // const pdfDoc = await PDFDocument.create()
+
+  //create from existing
+  const uint8Array = fs.readFileSync('./ESDC-EMP5624-E-DEMO.pdf')
   const pdfDoc = await PDFDocument.load(uint8Array)
+
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica)
+  const pages = pdfDoc.getPages()
+  const page1 = pages[0]
+
+  const { width, height } = page1.getSize()
+  console.log('height', height)
+  console.log('width', width)
+
+  // write data
+  writeFirstName(page1, font, data.firstName)
+  writeMiddleName(page1, font, data.middleName)
+  writeLastName(page1, font, data.lastName)
+  writeEmail(page1, font, data.email)
+
+  const pdfBytes = await pdfDoc.save()
+  fs.writeFileSync('./out/' + createFilename(data), pdfBytes)
 
   console.log('pdf created...')
 }
 
-index()
+const data = {
+  firstName: 'Jayson',
+  middleName: 'Ojaldon',
+  lastName: 'Nabor',
+  email: 'sonabstudios@gmail.com',
+  partner: {
+    firstName: 'Jayson1',
+    middleName: 'Ojaldon5',
+    lastName: 'Nabor3',
+    email: 'sonabstudios98@gmail.com'
+  }
+}
+
+index(data)
